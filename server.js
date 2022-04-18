032b7c66b15387de53f40ec59f7b45eac545ffa5
Thoma77s <thomassiffc3@gmail.com>
2022-04-18 11:43:17 -0400

    set up routes, and models

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+// const session = require('express-session');
+
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const sess = {
+//     secret: process.env.SESSION_SECRET,
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
